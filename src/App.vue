58764b61c1c02119720eea4639b24ac2dc9e4e97
dnalpert <36242436+dnalpert@users.noreply.github.com>
2019-11-20 21:33:40 -0800

    added dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+   <h1>Appointments</h1>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: 'MainApp',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
