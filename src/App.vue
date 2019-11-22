de6e4b112ee663b3aa5d599ac1cc0c2e6448aba5
dnalpert <36242436+dnalpert@users.noreply.github.com>
2019-11-22 09:32:56 -0800

    addItem functionality added

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,25 @@
     <div class="row justify-content-center">
       
       <h1>{{ title }}</h1>
+      <add-appointment
+        @add="addItem"  
+      ></add-appointment>
       <appointment-list 
         :appointments="appointments" 
         @remove="removeItem"
         @edit="editItem"  
       >
       </appointment-list>
-    
     </div>
   </div>
 
 </template>
 
 <script>
-import AppointmentList from "./components/AppointmentList"
-import _ from "lodash"
-import axios from "axios"
+import AppointmentList from "./components/AppointmentList";
+import AddAppointment from "./components/AddAppointment";
+import _ from "lodash";
+import axios from "axios";
 
 export default {
   name: "MainApp",
@@ -30,7 +33,8 @@ export default {
     };
   },
   components: {
-    AppointmentList
+    AppointmentList,
+    AddAppointment
   },
   mounted () {
     axios
@@ -50,6 +54,11 @@ export default {
         aptId: id
       });
       this.appointments[aptIndex][field] = text;
+    },
+    addItem: function (apt) {
+      apt.aptId= this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt)
     }
   }
 };
